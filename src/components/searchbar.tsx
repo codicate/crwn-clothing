312517a904b5074df8e39d342326e3a8b32ef525
codicate <codicate@gmail.com>
2021-03-31 15:52:49 -0400

    commented out for later typescriptification

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,36 +1,38 @@
-import React, { useState, useRef } from 'react';
+// import React, { useState, useRef } from 'react';
 
-export default function Searchbar(props) {
-  const [input, setInput] = useState('');
-  const searchbar = useRef();
+// export default function Searchbar(props) {
+//   const [input, setInput] = useState('');
+//   const searchbar = useRef();
 
-  return (
-    <div id='searchBar'>
-      <input
-        autoFocus
-        type='text'
-        placeholder='Search'
-        ref={searchbar}
-        value={input}
-        onChange={
-          e => setInput(e.target.value)
-        }
-        onKeyUp={e => 
-          e.key === 'Enter' && props.returnInput(e.target.value)
-        }
-      />
-      <div id='clearDiv'>
-        <span
-          id='clear'
-          className='material-icons btn'
-          onClick={() => {
-            setInput('');
-            searchbar.current.focus();
-          }}
-        >
-          clear
-        </span>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div id='searchBar'>
+//       <input
+//         autoFocus
+//         type='text'
+//         placeholder='Search'
+//         ref={searchbar}
+//         value={input}
+//         onChange={
+//           e => setInput(e.target.value)
+//         }
+//         onKeyUp={e => 
+//           e.key === 'Enter' && props.returnInput(e.target.value)
+//         }
+//       />
+//       <div id='clearDiv'>
+//         <span
+//           id='clear'
+//           className='material-icons btn'
+//           onClick={() => {
+//             setInput('');
+//             searchbar.current.focus();
+//           }}
+//         >
+//           clear
+//         </span>
+//       </div>
+//     </div>
+//   );
+// }
+
+export { };
