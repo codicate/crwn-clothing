f0bb4bacf6f07f697f2425097ba14b3b1e0caea8
codicate <codicate@gmail.com>
2021-04-05 13:15:29 -0400

    routing finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import 'App.scss';
 
 import Homepage from 'pages/Homepage';
+import Item from 'pages/Item';
 
-function App() { 
+function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/item/:itemName" component={Item} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,25 +1,29 @@
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import styles from 'components/MenuItem.module.scss';
 
-export default function MenuItem({ title, imageUrl, size }
-  : {
+const MenuItem = ({ title, imageUrl, size, history, match }:
+  {
     title: string,
     imageUrl: string,
     size?: string;
-  }
-) {
-  return (
-    <div className={styles.menuItem + ' ' + styles[`${size}`]}>
-      <div
-        className={styles.backgroundImage}
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className={styles.content}>
-        <h1 className={styles.title}>{title.toUpperCase()}</h1>
-        <span className={styles.subtitle}>SHOP NOW</span>
-      </div>
+  } & RouteComponentProps
+) => (
+  <div
+    className={styles.menuItem + ' ' + styles[`${size}`]}
+    onClick={() => history.push(`${match.url}item/${title}`)}
+  >
+    <div
+      className={styles.backgroundImage}
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className={styles.content}>
+      <h1 className={styles.title}>{title.toUpperCase()}</h1>
+      <span className={styles.subtitle}>SHOP NOW</span>
     </div>
-  );
-};
+  </div>
+);
+
+export default withRouter(MenuItem);
 
