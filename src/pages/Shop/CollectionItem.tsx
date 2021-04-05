5ff58813af467e24527ecc95c42720c4e850e342
codicate <codicate@gmail.com>
2021-04-05 16:54:21 -0400

    components done

--- src/pages/Shop/CollectionItem.tsx
+++ src/pages/Shop/CollectionItem.tsx
@@ -2,12 +2,25 @@ import styles from 'pages/Shop/CollectionItem.module.scss';
 
 import React from 'react';
 
-const CollectionItem = () => {
-  return (
-    <div>
-      CollectionItem
+const CollectionItem = ({ name, price, imageUrl }:
+  {
+    name: string;
+    price: number;
+    imageUrl: string;
+  }
+) => (
+  <div className={styles.collectionItem}>
+    <div
+      className={styles.image}
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className={styles.collectionFooter}>
+      <span className={styles.name}>{name}</span>
+      <span className={styles.price}>{price}</span>
     </div>
-  );
-};
+  </div>
+);
 
 export default CollectionItem;
--- src/pages/Shop/CollectionPreview.tsx
+++ src/pages/Shop/CollectionPreview.tsx
@@ -1,13 +1,27 @@
 import styles from 'pages/Shop/CollectionPreview.module.scss';
+import CollectionItem from 'pages/Shop/CollectionItem';
 
-import React from 'react';
-
-const CollectionPreview = () => {
-  return (
-    <div>
-      CollectionPreview
+const CollectionPreview = ({ title, items }:
+  {
+    title: string;
+    items: [{
+      id: number;
+      name: string;
+      price: number;
+      imageUrl: string;
+    }];
+  }
+) => (
+  <div className={styles.collectionPreview}>
+    <h1 className={styles.title}>{title.toUpperCase()}</h1>
+    <div className={styles.preview}>
+      {items
+        .filter((_, idx) => idx < 4)
+        .map(({ id, ...props }) => (
+          <CollectionItem key={id} {...props} />
+        ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default CollectionPreview;
