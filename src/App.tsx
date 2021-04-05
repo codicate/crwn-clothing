bc60e3968648a5416426c2ea62d2ecff5b9f6490
codicate <codicate@gmail.com>
2021-04-05 11:43:21 -0400

    installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import 'App.scss';
+
 import Homepage from 'pages/Homepage';
 
-function App() {
-  return <>
-
-    <Homepage />
-
-  </>;
+function App() { 
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
