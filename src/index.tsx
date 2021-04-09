592768bb8326b3526c3b01bf10e8f383e94693ef
codicate <codicate@gmail.com>
2021-04-08 22:55:29 -0400

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from 'app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from 'serviceWorker';
 import 'index.css';
+
+import { store } from 'app/store';
+import * as serviceWorker from 'serviceWorker';
 import App from 'App';
 
 ReactDOM.render(
