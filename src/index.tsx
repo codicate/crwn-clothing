b584f8bc2562eda7ad41091e80ba4b4f7f87f3da
codicate <codicate@gmail.com>
2021-04-08 22:48:58 -0400

    migrate to cra-redux-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from 'app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from 'serviceWorker';
 import 'index.css';
-
 import App from 'App';
-import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals();
+serviceWorker.unregister();
