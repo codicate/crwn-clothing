bc957d684d42af83c34c0c1ed078d8b77bc3908d
codicate <codicate@gmail.com>
2021-05-31 15:41:00 -0400

    more favicon fix

--- server.ts
+++ server.ts
@@ -6,14 +6,16 @@ import Stripe from 'stripe';
 
 
 const buildPath = 'client/build';
+const port = process.env.PORT || 5000;
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, buildPath)));
@@ -28,9 +30,6 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-console.log(import.meta.url)
-
-const port = process.env.PORT || 5000;
 app.listen(port);
 
 
